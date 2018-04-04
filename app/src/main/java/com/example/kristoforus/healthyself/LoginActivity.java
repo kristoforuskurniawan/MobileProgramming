e31b97ac1dbbbade984f3ed1dde8fd4923bd014a
kristoforuskurniawan <kristoforus10@yahoo.com>
2018-04-04 16:15:19 +0700

    Trying to implement Doctor features

--- app/src/main/java/com/example/kristoforus/healthyself/LoginActivity.java
+++ app/src/main/java/com/example/kristoforus/healthyself/LoginActivity.java
@@ -15,7 +15,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.RadioButton;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
